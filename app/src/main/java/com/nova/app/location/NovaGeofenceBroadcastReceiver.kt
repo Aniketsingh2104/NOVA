87cac5ddc2762e667d0205cb0de8a92ed2a63eb9
Aniketsingh2104 <mis@ginzalimited.com>
2026-06-27 01:59:15 +0530

    Add app/src/main/java/com/nova/app/location/NovaGeofenceBroadcastReceiver.kt

---
+++ app/src/main/java/com/nova/app/location/NovaGeofenceBroadcastReceiver.kt
@@ -0,0 +1,8 @@
+package com.nova.app.location
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NovaGeofenceBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
